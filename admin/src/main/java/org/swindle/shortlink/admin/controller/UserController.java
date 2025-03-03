a588d4472b9f71598e90d86babe4262753fe7112
AillemaCc <yzw1635994342@outlook.com>
2025-03-03 21:49:02 +0800

    Initial commit;用户部分： 数据持久层设计 异常设计 全局返回实体 全局异常拦截器

---
+++ admin/src/main/java/org/swindle/shortlink/admin/controller/UserController.java
@@ -0,0 +1,37 @@
+package org.swindle.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.swindle.shortlink.admin.common.convention.result.Result;
+import org.swindle.shortlink.admin.common.convention.result.Results;
+import org.swindle.shortlink.admin.common.enums.UserErrorCodeEnum;
+import org.swindle.shortlink.admin.dto.resp.UserRespDTO;
+import org.swindle.shortlink.admin.service.UserService;
+
+/*
+用户管理控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    /**
+     * 根据用户名查询用户信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortlink/admin/v1/user/{username}")
+    public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDTO result = userService.getUserByUsername(username);
+        if (result == null) {
+            return new Result<UserRespDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
+        }
+        else {
+            return Results.success(result);
+        }
+    }
+
+}
