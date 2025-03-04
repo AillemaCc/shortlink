7b6606b3e6d93bf24f0e53ce6f5adad00357e26b
AillemaCc <yzw1635994342@outlook.com>
2025-03-04 18:20:25 +0800

    feature：通过分布式锁完善用户注册功能，防止恶意请求短时间内注册

---
+++ admin/src/main/java/org/swindle/shortlink/admin/common/constant/RedisCatchConstant.java
@@ -0,0 +1,8 @@
+package org.swindle.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管redis缓存常量类
+ */
+public class RedisCatchConstant {
+    public static final String LOCK_USER_REGISTER_KEY="short-link:lock-user-register:";
+}
--- admin/src/main/java/org/swindle/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/swindle/shortlink/admin/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.swindle.shortlink.admin.common.convention.exception.ClientException;
@@ -26,7 +28,7 @@ import static org.swindle.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SA
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
-
+    private final RedissonClient redissonClient;
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -57,10 +59,19 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUsername(requestParam.getUsername())) {
             throw new ClientException(USER_NAME_EXIST);
         }
-        int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (insert < 1) {
-            throw new ClientException(USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock("LOCK_USER_REGISTER_KEY"+requestParam.getUsername());
+        try{
+            if(lock.tryLock()){
+                int insert = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (insert < 1) {
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+            }
+            throw new ClientException(USER_NAME_EXIST);
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+        finally {
+            lock.unlock();
+        }
     }
 }
