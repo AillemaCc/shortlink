706f0c24793d6a2989ae78d09eb7658a3c1092e9
AillemaCc <yzw1635994342@outlook.com>
2025-03-04 15:05:12 +0800

    feature：后管系统查询用户信息的脱敏or无脱敏

--- admin/src/main/java/org/swindle/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/swindle/shortlink/admin/controller/UserController.java
@@ -1,12 +1,15 @@
 package org.swindle.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.swindle.shortlink.admin.common.convention.result.Result;
 import org.swindle.shortlink.admin.common.convention.result.Results;
 import org.swindle.shortlink.admin.common.enums.UserErrorCodeEnum;
+import org.swindle.shortlink.admin.dto.resp.UserActualRespDTO;
 import org.swindle.shortlink.admin.dto.resp.UserRespDTO;
 import org.swindle.shortlink.admin.service.UserService;
 
@@ -32,6 +35,16 @@ public class UserController {
         else {
             return Results.success(result);
         }
+    }
+
+    /**
+     * 根据用户名查询无脱敏信息
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortlink/admin/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable("username") String username) {
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 
 }
