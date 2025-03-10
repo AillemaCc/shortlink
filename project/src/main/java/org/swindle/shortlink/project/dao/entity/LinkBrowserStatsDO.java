7dcb5a431798c12ede4d41462382a435ca7f5b21
AillemaCc <yzw1635994342@outlook.com>
2025-03-10 11:10:36 +0800

    feature:开发短链接监控-浏览器访问

---
+++ project/src/main/java/org/swindle/shortlink/project/dao/entity/LinkBrowserStatsDO.java
@@ -0,0 +1,49 @@
+package org.swindle.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.swindle.shortlink.project.common.database.BaseDO;
+
+import java.util.Date;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_link_browser_stats")
+public class LinkBrowserStatsDO extends BaseDO {
+    @TableId(type = IdType.AUTO)
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+}
